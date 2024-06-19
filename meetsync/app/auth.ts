e0d3396ee2b0f7be67c0ab80f57773a8759c07b6
Lukas Zainzinger <lzainzinger@Air-von-Lukas.home>
2024-06-19 21:36:11 +0200

    added env access to auth provider

--- meetsync/app/auth.ts
+++ meetsync/app/auth.ts
@@ -1,7 +1,13 @@
 
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+
+const authOptions = {
+  providers: [Google({
+    clientId: process.env.AUTH_GOOGLE_ID,
+    clientSecret: process.env.AUTH_GOOGLE_SECRET
+  })],
+  secret: process.env.AUTH_SECRET
+}
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-})
+export const { handlers, signIn, signOut, auth } = NextAuth(authOptions)
